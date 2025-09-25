26a8b4318b0a397b13d5b636962408033e2b2755
Atharv-web <aargangodkar@gmail.com>
2025-09-25 16:53:27 +0530

    modifying file for production

--- backend/tools/kb_tool.py
+++ backend/tools/kb_tool.py
@@ -4,6 +4,8 @@ from llama_index.core import VectorStoreIndex, StorageContext,Settings, load_ind
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.readers.file import PyMuPDFReader
+from dotenv import load_dotenv
+load_dotenv()
 
 # ollama_embedding = OllamaEmbedding(
 #     model_name="nomic-embed-text:latest",
@@ -12,24 +14,44 @@ from llama_index.readers.file import PyMuPDFReader
 
 # Settings.embed_model = ollama_embedding
 
-# def encode_db():
-#     base_dir = os.path.dirname(os.path.abspath(__file__))
-#     pdf_path = os.path.join(base_dir,"../data/mathbook.pdf")
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+# persist_dir = os.path.join(os.path.dirname(__file__), "db", "VectorDB")
+# faiss_index_file = os.path.join(persist_dir, "faiss.index")
 
+# def encode_db():
+#     # pdf_path = os.path.join(base_dir,"../data/mathbook.pdf")
+#     os.makedirs(persist_dir, exist_ok=True)
+#     pdf_path = os.path.join(base_dir,"../data/mathbook.pdf")
 #     documents = PyMuPDFReader().load(pdf_path)
     
 #     dimensions = 768
 #     faiss_index = faiss.IndexFlatL2(dimensions)
 #     vector_store = FaissVectorStore(faiss_index=faiss_index)
 #     storage_context = StorageContext.from_defaults(vector_store=vector_store)
-#     vector_store.persist(persist_path="./VectorDB")
 
 #     # Store in Faiss
 #     index = VectorStoreIndex.from_documents(documents,storage_context=storage_context)
-#     index.storage_context.persist(persist_dir="./VectorDB")
-    # return index
+#     vector_store.persist(persist_path=persist_dir)
+#     index.storage_context.persist(persist_dir=persist_dir)
+#     return index
 
-storage_context = StorageContext.from_defaults(persist_dir="./VectorDB")
+# if os.path.exists(os.path.join(persist_dir,"docstore.json")):
+#     storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
+#     index = load_index_from_storage(storage_context=storage_context)
+# else:
+#     index = encode_db()
+
+# queryEngine = index.as_query_engine
+
+# rag_tool = QueryEngineTool(
+#     query_engine=queryEngine,
+#     metadata=ToolMetadata(
+#         name="RAG-Tool",
+#         description="RAG tool for answering mathematical questions from the pdf vector store."
+#     ),
+# )
+
+storage_context = StorageContext.from_defaults(persist_dir="../VectorDB")
 index = load_index_from_storage(storage_context=storage_context)
 query_engine = index.as_query_engine()
 
