b7e3a46c60febf1cbedfc04ea88e566af39776d8
Atharv-web <aargangodkar@gmail.com>
2025-09-27 21:33:56 +0530

    making changes for deployment

--- backend/kb_tool.py
+++ backend/kb_tool.py
@@ -4,8 +4,6 @@ from llama_index.core import VectorStoreIndex, StorageContext,Settings, load_ind
 from llama_index.vector_stores.faiss import FaissVectorStore
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.readers.file import PyMuPDFReader
-from dotenv import load_dotenv
-load_dotenv()
 
 embedding_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
 Settings.embed_model = embedding_model
