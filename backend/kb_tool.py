f326c322560ece90a350c01794bb6828ce6b4ae7
Atharv-web <aargangodkar@gmail.com>
2025-09-28 21:52:40 +0530

    making backend lightweight

--- backend/kb_tool.py
+++ backend/kb_tool.py
@@ -1,11 +1,9 @@
-import faiss,os
+import os
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import VectorStoreIndex, StorageContext,Settings, load_index_from_storage
-from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.core import VectorStoreIndex, StorageContext,Settings, load_index_from_storage,SimpleDirectoryReader
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
-from llama_index.readers.file import PyMuPDFReader
 
-embedding_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.embed_model = embedding_model
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,21 +12,13 @@ persist_dir_path = os.path.join(base_dir,"db","VectorStore")
 pdf_path = os.path.join(base_dir,"data","mathbook.pdf")
 
 def encode_db(db_path):
-    documents = PyMuPDFReader().load(db_path)
-    
-    dimensions = 384
-    faiss_index = faiss.IndexFlatL2(dimensions)
-    vector_store = FaissVectorStore(faiss_index=faiss_index)
-    storage_context = StorageContext.from_defaults(vector_store=vector_store)
-
-    # Store in Faiss
-    index = VectorStoreIndex.from_documents(documents,storage_context=storage_context)
+    documents= SimpleDirectoryReader(input_files=[db_path]).load_data()
+    index = VectorStoreIndex.from_documents(documents,embed_model=embedding_model)
     index.storage_context.persist(persist_dir=persist_dir_path)
     return index
 
 if os.path.exists(os.path.join(persist_dir_path,"docstore.json")):
-    vector_store= FaissVectorStore.from_persist_dir(persist_dir_path)
-    storage_context = StorageContext.from_defaults(persist_dir=persist_dir_path,vector_store=vector_store)
+    storage_context = StorageContext.from_defaults(persist_dir=persist_dir_path)
     index = load_index_from_storage(storage_context=storage_context)
 else:
     index = encode_db(pdf_path)
